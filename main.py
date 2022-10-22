ca0aa2c591eea365976a6ddbedd048b499eb762c
Artysparty <artyom.kozlov.2000@bk.ru>
2022-10-22 13:34:37 +0300

    add geodata

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import pandas as pd
-import geopandas as gpd
-from shapely.geometry import Point
 import matplotlib.pyplot as plt
-import seaborn as sns
+import geopandas as gpd
+from shapely.geometry import LineString
+from shapely.ops import split
+from shapely.affinity import translate
 
 data = pd.read_csv("./venv/data/thermopoints.csv")
 pd.core.frame.DataFrame
 pd.set_option("display.max.columns", None)
-data.head()
 
 # Количество лесных пожаров по видам
 plt.plot(data.type_name.unique(), data.type_name.value_counts())
@@ -31,4 +31,48 @@ plt.plot(data.year.unique(), [data.loc['2012'].year.value_counts(),
                               data.loc['2020'].year.value_counts(),
                               data.loc['2021'].year.value_counts()
                               ])
+plt.show()
+
+# Метод сдвига координат
+def shift_geom(shift, gdataframe):
+    shift -= 180
+    moved_map = []
+    splitted_map = []
+    border = LineString([(shift,90),(shift,-90)])
+
+    for row in gdataframe["geometry"]:
+        splitted_map.append(split(row, border))
+    for element in splitted_map:
+        items = list(element)
+        for item in items:
+            minx, miny, maxx, maxy = item.bounds
+            if minx >= shift:
+                moved_map.append(translate(item, xoff=-180-shift))
+            else:
+                moved_map.append(translate(item, xoff=180-shift))
+
+    gdf = gpd.GeoDataFrame({"geometry": moved_map})
+    # if plotQ:
+    #     fig, ax = plt.subplots()
+    #     gdf.plot(ax=ax)
+    #     plt.show()
+
+    return gdf
+
+# Проекция координат пожаров за 2012 год на карту России
+small_data = data.loc[data.year == 2012]
+gdf = gpd.GeoDataFrame(
+    small_data,
+    geometry=gpd.points_from_xy(pd.to_numeric(small_data['lon'], errors='coerce').fillna(0, downcast='infer'),
+                                pd.to_numeric(small_data['lat'], errors='coerce').fillna(0, downcast='infer')))
+
+world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+russia = world[world['name'] == 'Russia']
+
+rus_shift_90 = shift_geom(90, russia)
+good_geom_rus = shift_geom(-90, rus_shift_90)
+
+fig, ax = plt.subplots(figsize=(10,10))
+good_geom_rus.plot(ax=ax, color='lightgrey', zorder=1)
+gdf.plot(ax=ax, alpha=0.5, zorder=2)
 plt.show()
